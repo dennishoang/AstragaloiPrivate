322352a30903e6e6a3427c9b35d61fa5a1483fa8
Neoyx <noah.leuft@gmail.com>
2022-10-26 21:15:10 +0200

    Spec

--- src/main/scala/de/htwg/se/astragaloi/model/Field.scala
+++ src/main/scala/de/htwg/se/astragaloi/model/Field.scala
@@ -3,11 +3,12 @@ package model
 class Field {
 
 val eol = sys.props("line.separator");
-def bars(cellWidth: Int = 5, cellspace: Int = 5, space: Int = 20) =
+def bars(cellWidth: Int, cellspace: Int, space: Int) =
   " " * space + (("+" + "-" * cellWidth) + "+" + " " * cellspace) * 3 + eol
-def cells(cellWidth: Int = 5, cellspace: Int = 5, space: Int = 20) =
+def cells(cellWidth: Int, cellspace: Int, space: Int) =
   " " * space + (("|" + " " * cellWidth) + "|" + " " * cellspace) * 3 + eol
-def playfield() = (bars() + (cells() * 3)) * 3 + bars()
+def playfield(cellWidth: Int = 5, cellspace: Int = 5, space: Int = 20) =
+  (bars(cellWidth, cellspace, space) + (cells(cellWidth, cellspace, space) * 3)) * 3 + bars(cellWidth, cellspace, space)
 
 def quadbar(width: Int, scale: Int) = " " * scale + "+" + ("-" * width) + "+" + eol
 def quadcell(width: Int, scale: Int) = " " * scale + "|" + (" " * width) + "|" + eol
--- src/test/scala/de/htwg/se/astragaloi/model/FieldSpec.scala
+++ src/test/scala/de/htwg/se/astragaloi/model/FieldSpec.scala
@@ -1,6 +1,20 @@
 package de.htwg.se.astragaloi.model
 
-import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.matcher.should.Matchers._
+import model.Field
 
-class Field extends AnyWordSpec {}
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+
+class FieldSpec extends AnyWordSpec {
+
+    "A Field" when {
+        "empty " should {
+            "be created by using a dimension and a simple cell" in {
+                val field = new Field
+                field.playfield(10,10,10) should be(2)
+            }
+        }
+    }
+}
+
