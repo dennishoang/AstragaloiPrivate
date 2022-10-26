fb5b8df4df8c12a6e3779829812a6cf11d34c2db
Neoyx <noah.leuft@gmail.com>
2022-10-26 21:25:14 +0200

    scale to space

--- src/main/scala/de/htwg/se/astragaloi/model/Field.scala
+++ src/main/scala/de/htwg/se/astragaloi/model/Field.scala
@@ -10,9 +10,9 @@ def cells(cellWidth: Int, cellspace: Int, space: Int) =
 def playfield(cellWidth: Int = 5, cellspace: Int = 5, space: Int = 20) =
   (bars(cellWidth, cellspace, space) + (cells(cellWidth, cellspace, space) * 3)) * 3 + bars(cellWidth, cellspace, space)
 
-def quadbar(width: Int, scale: Int) = " " * scale + "+" + ("-" * width) + "+" + eol
-def quadcell(width: Int, scale: Int) = " " * scale + "|" + (" " * width) + "|" + eol
-def quadrat(width: Int, length: Int, scale: Int) = (quadbar(width, scale) + (quadcell(width, scale) * length)) + quadbar(width, scale)
+def quadbar(width: Int, space: Int) = " " * space + "+" + ("-" * width) + "+" + eol
+def quadcell(width: Int, space: Int) = " " * space + "|" + (" " * width) + "|" + eol
+def quadrat(width: Int, length: Int, space: Int) = (quadbar(width, space) + (quadcell(width, space) * length)) + quadbar(width, space)
 
 val mesh = quadrat(12, 6, 60) + eol + playfield() + eol + "-" * 80 + eol * 2 + playfield() + quadrat(12, 6, 0)
 
