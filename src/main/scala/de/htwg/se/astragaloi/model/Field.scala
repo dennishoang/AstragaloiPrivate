a9e044c36ea54564d4c8d4bc2c88cf7116e2a31b
Neoyx <noah.leuft@gmail.com>
2022-10-27 21:16:57 +0200

    updated mesh

--- src/main/scala/de/htwg/se/astragaloi/model/Field.scala
+++ src/main/scala/de/htwg/se/astragaloi/model/Field.scala
@@ -3,18 +3,21 @@ package model
 class Field {
 
 val eol = sys.props("line.separator");
-def bars(cellWidth: Int, cellspace: Int) =
-  (("+" + "-" * cellWidth) + "+" + " " * cellspace) * 3 + eol
-def cells(cellWidth: Int, cellspace: Int) =
-  (("|" + " " * cellWidth) + "|" + " " * cellspace) * 3 + eol
-def playfield(cellWidth: Int = 5, cellspace: Int = 5, celllength: Int = 3) =
-  (bars(cellWidth, cellspace) + (cells(cellWidth, cellspace) * celllength)) * 3 + bars(cellWidth, cellspace)
+def bars(width: Int, space: Int) =
+  (("+" + "-" * width) + "+" + " " * space) * 3 + eol
+def cells(width: Int, space: Int) =
+  (("|" + " " * width) + "|" + " " * space) * 3 + eol
+def playfield(width: Int = 5, length: Int = 3, space: Int = 5) =
+  (bars(width, space) + (cells(width, space) * length)) * 3 + bars(width, space)
 
 def quadbar(width: Int, space: Int) = " " * space + "+" + ("-" * width) + "+" + eol
 def quadcell(width: Int, space: Int) = " " * space + "|" + (" " * width) + "|" + eol
 def quadrat(width: Int, length: Int, space: Int) = (quadbar(width, space) + (quadcell(width, space) * length)) + quadbar(width, space)
 
-def mesh(seperator: Int = 60, quadwidth: Int = 12, quadlength: Int = 6, quadspace: Int = 40) =
-  quadrat(quadwidth, quadlength, quadspace) + eol + playfield() + eol + "-" * seperator + eol * 2 + playfield() + eol + quadrat(quadwidth, quadlength, quadspace)
+def mesh(seperator: Int = 60, quadwidth: Int = 12, quadlength: Int = 6, quadspace: Int = 40,
+  cellWidth: Int = 5, celllength: Int = 3, cellspace: Int = 5) =
+  quadrat(quadwidth, quadlength, quadspace) + eol + playfield(cellWidth, celllength, cellspace)
+  + eol + "-" * seperator + eol * 2
+  + playfield(cellWidth, celllength, cellspace) + eol + quadrat(quadwidth, quadlength, quadspace)
 
 }
