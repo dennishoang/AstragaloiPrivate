8ba69dd48a2276a68c57cec200db4ac4b8c628e1
Neoyx <noah.leuft@gmail.com>
2022-11-02 20:35:56 +0100

    MatrixSpec

--- src/test/scala/de/htwg/se/astragaloi/model/MatrixSpec.scala
+++ src/test/scala/de/htwg/se/astragaloi/model/MatrixSpec.scala
@@ -7,14 +7,23 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class MatrixSpec extends AnyWordSpec {
+    val matrix = new Matrix[Dice](2, Dice.ONE)
     "A Matrix" should {
         "be created by using a dimention and a cell" in {
-            val matrix = new Matrix[String](2, "3")
             matrix.size should be(2)
         }
         "for test purposes" in {
             val matrix2 = new Matrix[Dice](2, Dice.ONE)
             matrix2.cell(0,0) should be(Dice.ONE)
+        }
+        "replace cells" in {
+            val matrix3 = matrix.replaceCell(0, 0, Dice.TWO)
+            matrix.cell(0,0) should be(Dice.ONE)
+            matrix3.cell(0,0) should be(Dice.TWO)
+        }
+        "be filled" in {
+            val matrix4 = matrix.fill(Dice.Empty)
+            matrix4.cell(0,0) should be(Dice.Empty)
         }
     }
 }
