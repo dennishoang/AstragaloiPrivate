19d12eb3e0ae40d1021276ef0d90f95420780d4a
Neoyx <noah.leuft@gmail.com>
2022-11-02 12:41:33 +0100

    Dice

--- src/test/scala/de/htwg/se/astragaloi/model/DiceSpec.scala
+++ src/test/scala/de/htwg/se/astragaloi/model/DiceSpec.scala
@@ -5,3 +5,10 @@ package de.htwg.se.astragaloi.model
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
+class DiceSpec extends AnyWordSpec {
+    "A Dice should" {
+        val dice = Dice()
+        "have a set of numbers from 1 to 6"
+    }
+
+}
