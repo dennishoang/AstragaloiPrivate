0c7995d620df7f0d973e9588acbdab733bba5148
Neoyx <noah.leuft@gmail.com>
2022-11-03 22:38:17 +0100

    Main updated

--- src/main/scala/de/htwg/se/astragaloi/Main.scala
+++ src/main/scala/de/htwg/se/astragaloi/Main.scala
@@ -1,15 +1,46 @@
 import model.TUI
 import model.Dice
+import scala.io.StdIn.readLine
 
 
 @main def game: Unit =
-  val field = new TUI(3, 2, Dice.Empty)
+  /* val field = new TUI(3, 2, Dice.Empty)
   println(field.toString)
   val field2 = field.put(Dice.ONE, 0, 0, 0)
   val field3 = field.put(Dice.TWO, 1, 0, 1)
   println(field2.toString)
   println(field3.toString)
   val field4 = field.putSlot(Dice.SIX, 0)
-  println(field4.toString)
+  println(field4.toString) */
+  println("Welcome to Astragaloi")
+  val field = new TUI(3, 2, Dice.Empty)
+  println(field.toString)
+  getInputAndPrintLoop(field)
 
+def getInputAndPrintLoop(field: TUI): Unit =
+  val input = readLine
+  parseInput(input) match
+    case None => field
+    case Some(newfield) =>
+      println(newfield)
+      getInputAndPrintLoop(newfield)
 
+  def parseInput(input: String): Option[TUI] =
+    input match
+      case "q" => None
+      case _ => {
+        val chars = input.toCharArray
+        val dice = chars(0) match
+          case '1' => Dice.ONE
+          case '2' => Dice.TWO
+          case '3' => Dice.THREE
+          case '4' => Dice.FOUR
+          case '5' => Dice.FIVE
+          case '6' => Dice.SIX
+          case _   => Dice.Empty
+        val matrix = chars(1).toString.toInt
+        val x = chars(2).toString.toInt
+        val y = chars(3).toString.toInt
+        Some(field.put(dice, matrix, x, y))
+
+      }
