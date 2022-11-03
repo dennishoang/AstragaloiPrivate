c550158c26cecb076fce46561fa903ee3c74e886
dennishoang <dennishoang199@gmail.com>
2022-11-03 19:21:36 +0100

    Branch SE_03

--- src/main/scala/de/htwg/se/astragaloi/Main.scala
+++ src/main/scala/de/htwg/se/astragaloi/Main.scala
@@ -1,14 +1,8 @@
-import model.Field2
-import model.Dice
+import model.Field
 
-//import scala.annotation.meta.field.apply
 
 @main def hello: Unit =
-  val field = new Field2(3, Dice.Empty)
-  println(field.toString)
-  val field2 = field.put(Dice.ONE, 0, 0, 0)
-  val field3 = field.put(Dice.TWO, 1, 0, 1)
-  println(field2.toString)
-  println(field3.toString)
+  val field = new Field()
+  println(field.mesh())
 
 
--- src/main/scala/de/htwg/se/astragaloi/model/Matrix.scala
+++
@@ -1,9 +0,0 @@
-package model
-
-case class Matrix[T](rows: Vector[Vector[T]], player: Int):
-    def this(size: Int, filling: T, player: Int) = this(Vector.tabulate(size, size) { (row, col) => filling }, player)
-    val size: Int = rows.size
-    def cell(row: Int, col: Int): T = rows(row)(col) // z.B: rows(3)(1): Zugriff auf erste Zelle Spalte 3
-    def row(row: Int) = rows(row)
-    def fill(filling: T): Matrix[T] = copy(Vector.tabulate(size, size) { (row, col) => filling })
-    def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(rows.updated(row, rows(row).updated(col, cell)))
