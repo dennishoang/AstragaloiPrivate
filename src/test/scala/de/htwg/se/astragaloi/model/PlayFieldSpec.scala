7dddb4d0d3af3c2b14614338801423a2bccf3fd1
Neoyx <noah.leuft@gmail.com>
2022-11-03 19:30:28 +0100

    changed

--- src/test/scala/de/htwg/se/astragaloi/model/PlayFieldSpec.scala
+++ src/test/scala/de/htwg/se/astragaloi/model/PlayFieldSpec.scala
@@ -7,23 +7,23 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class PlayFieldSpec extends AnyWordSpec {
-    val matrix = new PlayField[Dice](2, Dice.ONE)
-    "A Matrix" should {
+    val playfield = new PlayField[Dice](2, Dice.ONE)
+    "A PlayField" should {
         "be created by using a dimention and a cell" in {
-            matrix.size should be(2)
+            playfield.size should be(2)
         }
         "for test purposes" in {
-            val matrix2 = new Matrix[Dice](2, Dice.ONE)
-            matrix2.cell(0,0) should be(Dice.ONE)
+            val playfield2 = new PlayField[Dice](2, Dice.ONE)
+            playfield2.cell(0, 0, 0) should be(Dice.ONE)
         }
         "replace cells" in {
-            val matrix3 = matrix.replaceCell(0, 0, Dice.TWO)
-            matrix.cell(0,0) should be(Dice.ONE)
-            matrix3.cell(0,0) should be(Dice.TWO)
+            val playfield3 = playfield.replaceCell(1, 1, 1, Dice.TWO)
+            playfield.cell(1, 1, 1) should be(Dice.ONE)
+            playfield3.cell(1, 1, 1) should be(Dice.TWO)
         }
         "be filled" in {
-            val matrix4 = matrix.fill(Dice.Empty)
-            matrix4.cell(0,0) should be(Dice.Empty)
+            val playfield4 = playfield.fill(Dice.Empty)
+            playfield4.cell(0, 0, 0) should be(Dice.Empty)
         }
     }
 }
