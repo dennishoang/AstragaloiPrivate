d54721127a8b5bb5eb20f17a5b8dd0c247eeaebf
root <root@DESKTOP-NKU3TOL.localdomain>
2022-11-23 14:36:51 +0100

    shape compositum finished

--- src/main/scala/de/htwg/se/astragaloi/model/Field.scala
+++ src/main/scala/de/htwg/se/astragaloi/model/Field.scala
@@ -12,30 +12,7 @@ case class Field(playfield: PlayField[Dice], diceslot: DiceSlot[Dice], pointslot
 
   def this(matrix_size: Int, diceslot_size: Int = 2, filling: Dice, fillingpoint: Int) =
     this(new PlayField(matrix_size, filling), new DiceSlot(diceslot_size, filling), new PointSlot(diceslot_size, fillingpoint))
-  val eol = sys.props("line.separator");
-
-
-  /*
-  def bars(width: Int, space: Int) = (("+" + "-" * width) + "+" + " " * space) * 3 + eol
-  def cells(matrix: Int, row: Int, width: Int, space: Int) =
-    playfield.row(matrix, row).map(_.toString).map("|" + " " * ((width - 1) / 2) + _ + " " * ((width - 1) / 2) + "|").mkString(" " * space) + eol
-  def playerfield(matrix: Int, width: Int, length: Int, space: Int) =
-    (0 until size).map(cells(matrix,_, width, space) * length).mkString(bars(width, space),bars(width, space),bars(width, space))
-
-  def pointcell(slot: Int, width: Int, space: Int) = pointslot.slot(slot).map(_.toString).map("|" + " " * ((width - 1) / 2) + _ + " " * ((width - 1) / 2) + "|").mkString(" " * space) + eol
-
-  def quadbar(width: Int, space: Int) = " " * space + "+" + ("-" * width) + "+" + eol
-  def quadcell(slot: Int, width: Int, space: Int) =
-    " " * space + "|" + " " * ((width - 1) / 2) + diceslot.cell(slot).toString + " " * ((width - 1) / 2) + "|" + eol
-  def quadrat(slot: Int, width: Int, length: Int, space: Int) =
-    (quadbar(width, space) + (quadcell(slot, width, space) * length)) + quadbar(width, space)
-
-  def mesh(seperator: Int = 25, quadwidth: Int = 5, quadlength: Int = 1, quadspace: Int = 15,
-    cellWidth: Int = 3, celllength: Int = 1, cellspace: Int = 1) =
-    quadrat(0, quadwidth, quadlength, quadspace) + eol + playerfield(0, cellWidth, celllength, cellspace)
-    + eol + pointcell(0, cellWidth, cellspace) + "-" * seperator + eol + pointcell(1, cellWidth, cellspace) + eol
-    + playerfield(1, cellWidth, celllength, cellspace) + eol + quadrat(1, quadwidth, quadlength, quadspace)
-  */
+  
   val mesh = new CompositeShape(
      new Quadrat[Dice](diceslot, 5, 15, 1, 0),
      new CellMatrix[Dice](playfield, 3, 1, 1, 3, 0),
--- src/main/scala/de/htwg/se/astragaloi/model/Shape.scala
+++ src/main/scala/de/htwg/se/astragaloi/model/Shape.scala
@@ -13,39 +13,37 @@ trait Shape:
 
 case class CellMatrix[T](playfield: PlayField[T], width: Int, space: Int, length: Int, size: Int, matrix: Int) extends Shape:
     override def create(): String =
-    	(0 until size).map(cells(_) * length).mkString(bars(),bars(),bars()) + eol
+    	(0 until size).map(cells(_) * length).mkString(bars(),bars(),bars())
 
     def bars(): String =
-        (("+" + "-" * width) + "+" + " " * space) * 3 + eol
+        (("+" + "-" * width) + "+" + " " * space) * 3
     def cells(row: Int): String =
-        playfield.row(matrix, row).map(_.toString).map("|" + " " * ((width - 1) / 2) + _ + " " * ((width - 1) / 2) + "|").mkString(" " * space) + eol
-
+        eol + playfield.row(matrix, row).map(_.toString).map("|" + " " * ((width - 1) / 2) + _ + " " * ((width - 1) / 2) + "|").mkString(" " * space) + eol
 
 
 case class Quadrat[T](diceslot: DiceSlot[T], width: Int, space: Int, length: Int, matrix: Int) extends Shape:
     override def create(): String =
-        (bars() + (cells() * length)) + bars() + eol
+        (bars() + (cells() * length)) + bars()
 
     def bars(): String =
-        " " * space + "+" + ("-" * width) + "+" + eol
+        " " * space + "+" + ("-" * width) + "+"
 
     def cells(): String =
-        " " * space + "|" + " " * ((width - 1) / 2) + diceslot.cell(matrix).toString + " " * ((width - 1) / 2) + "|" + eol
+        eol + " " * space + "|" + " " * ((width - 1) / 2) + diceslot.cell(matrix).toString + " " * ((width - 1) / 2) + "|" + eol
 
 
 case class PointCell[T](pointslot: PointSlot[T], width: Int, space: Int, matrix: Int) extends Shape:
     override def create(): String =
-        pointslot.slot(matrix).map(_.toString).map("|" + " " * ((width - 1) / 2) + _ + " " * ((width - 1) / 2) + "|").mkString(" " * space) + eol
+        pointslot.slot(matrix).map(_.toString).map("|" + " " * ((width - 1) / 2) + _ + " " * ((width - 1) / 2) + "|").mkString(" " * space)
 
-
+        
 case class MatrixSeperator(width: Int) extends Shape:
     override def create(): String =
-        "-" * width + eol
+        "=" * width
 
 
 class CompositeShape(shapes: Shape*) extends Shape:
     override def create(): String =
         val b = new StringBuilder()
-        //shapes.map(_.create()).mkString(eol).addString()
-        shapes.map(_.create()).addString(b)
+        shapes.map(_.create()).mkString(eol * 2).addString(b)
         b.toString()
