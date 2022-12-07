b4589c2186e59dc4d43f4b82d1317e41391f2e6b
Neoyx <noah.leuft@gmail.com>
2022-12-07 13:32:54 +0100

    undotest

--- src/test/scala/de/htwg/se/astragaloi/model/PlayFieldSpec.scala
+++ src/test/scala/de/htwg/se/astragaloi/model/PlayFieldSpec.scala
@@ -59,6 +59,13 @@ class PlayFieldSpec extends AnyWordSpec {
             playfield9 = playfield9.undestroyValue(1, 0, Dice.ONE, oldIndexes)
             playfield9.cell(0, 0, 0) should be (Dice.ONE)
         }
+        "undo inputs" in {
+            var playfield9 = new PlayField[Dice](3, Dice.Empty)
+            val oldIndexes = Vector[Int]()
+            playfield9 = playfield9.insertValue(playfield9, 0, 0, Dice.TWO, Dice.Empty, 0, oldIndexes) // with do
+            playfield9 = playfield9.insertValue(playfield9, 0, 0, Dice.TWO, Dice.Empty, 1, oldIndexes) // with undo
+            playfield9.cell(0, 0, 0) should be (Dice.Empty)
+        }
     }
 }
 
